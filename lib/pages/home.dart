5e0c72ed8e2189fec675f6a431bdab4df5415237
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-01 15:55:08 +0200

    create header widget

---
+++ lib/pages/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: 2,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              color: Colors.red,
+              height: 20,
+              width: 10,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/header.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class HeaderWidget extends StatelessWidget {
+  final int index;
+
+  const HeaderWidget({Key? key, required this.index}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      height: 120.0,
+      child: Card(
+        elevation: 8.0,
+        color: Colors.white,
+        // shape: const StadiumBorder(),
+        // shape: const UnderlineInputBorder(
+        //   borderSide: BorderSide(color: Colors.deepOrange),
+        // ),
+        shape: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.deepOrange.withOpacity(0.5)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              'Barista',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 48.0,
+                color: Colors.orange,
+              ),
+            ),
+            Text(
+              'Travel Plans',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
